fab83d65dcc6323ff10c5940c12b8433aff118ab
Wayou <liuwayong@gmail.com>
2013-07-12 08:56:14 +0800

    Clean code
    and display subject in the main page

--- js/wallist.js
+++ js/wallist.js
@@ -51,7 +51,6 @@ function removeById(id){
     var targetEvent;
    $.each(events, function(i, item) {
     if(item.id==id){
-        debugger;
         item.status=1;
         item.dateDone=new Date();
         storeEvents(events);
@@ -71,7 +70,6 @@ $(function() {
     // storeEvents(sampleData);
 
     events=$.parseJSON(localStorage.getItem("events"));
-debugger;
 //initial application, read existing events into the page
 if (events!==null) {
     for (var i = events.length - 1; i >= 0; i--) {
@@ -85,28 +83,28 @@ if (events!==null) {
     $("#0").offset();
       px=getRandomInt($("#0").offset().left,$("#0").offset().left+$("#0").width()-100);
      py=getRandomInt($("#0").offset().top,$("#0").offset().top+$("#0").height()-100)+10;
-     eventDom="<div class='event' id='"+events[i].id+"' style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
+     eventDom="<div class='event' id='"+events[i].id+"' title='"+events[i].detail+"' style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
     +events[i].subject+"<div class='deleteEvent'>X</div>"+"</div>";
     $("#0").append(eventDom);
     break;
      case "1":
        px=getRandomInt($("#1").offset().left,$("#1").offset().left+$("#1").width()-100);
      py=getRandomInt($("#1").offset().top,$("#1").offset().top+$("#1").height()-100)+10;
-     eventDom="<div class='event' id='"+events[i].id+"' style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
+     eventDom="<div class='event' id='"+events[i].id+"'title='"+events[i].detail+"'  style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
     +events[i].subject+"<div class='deleteEvent'>X</div>"+"</div>";
     $("#1").append(eventDom);
     break;
      case "2":
        px=getRandomInt($("#2").offset().left,$("#2").offset().left+$("#2").width()-100);
      py=getRandomInt($("#2").offset().top,$("#2").offset().top+$("#2").height()-100)+10;
-     eventDom="<div class='event' id='"+events[i].id+"' style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
+     eventDom="<div class='event' id='"+events[i].id+"'title='"+events[i].detail+"'  style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
     +events[i].subject+"<div class='deleteEvent'>X</div>"+"</div>";
     $("#2").append(eventDom);
     break;
      case "3":
        px=getRandomInt($("#3").offset().left,$("#3").offset().left+$("#3").width()-100);
      py=getRandomInt($("#3").offset().top,$("#3").offset().top+$("#3").height()-100)+10;
-     eventDom="<div class='event' id='"+events[i].id+"' style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
+     eventDom="<div class='event' id='"+events[i].id+"' title='"+events[i].detail+"' style='position:absolute;left:"+px+"px;top:"+py+"px;'>"
     +events[i].subject+"<div class='deleteEvent'>X</div>"+"</div>";
     $("#3").append(eventDom);
     break;
@@ -198,8 +196,23 @@ $(".navBtn").button().click(function(e){
         }
 	});
 	$(".section").droppable({
+         over: function( event, ui ) {
+$(this).addClass("dragIn");
+         },
+          out: function( event, ui ) {
+$(this).removeClass("dragIn");
+         },
 		drop: function(event, ui) {
-			console.log("dropped!")
+$(this).removeClass("dragIn");
+var id=$(this).attr("id");
+var eventId= ui.draggable.attr("id");
+$.each(events,function(i,item){
+    if (item.id==eventId) {
+        item.importance=id;
+        storeEvents(events);
+    };
+
+}) 
 		}
 	});
 
